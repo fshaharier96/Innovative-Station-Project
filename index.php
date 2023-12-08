39a0154ab90b753ae4f9e8d11defb8e309a0a8ee
saqlainmusi <rakibshaharier96@gmail.com>
2023-12-08 12:48:09 +0600

    stylesheet file  has been  added

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- fontawsome cdn -->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <!-- bootstrap css file -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- stylesheet link-->
+    <link rel="stylesheet" href="assets/style/css/main.css">
+
+    <title>Document</title>
+</head>
+<body>
+
+<!-- javascript dependencies -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
